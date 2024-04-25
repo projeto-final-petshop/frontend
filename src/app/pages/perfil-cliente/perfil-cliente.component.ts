cff459df2471fe50393b69911e5b003a3d23d525
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-04-25 19:08:14 -0300

    mock frontend

--- src/app/pages/perfil-cliente/perfil-cliente.component.ts
+++ src/app/pages/perfil-cliente/perfil-cliente.component.ts
@@ -33,6 +33,7 @@ export class PerfilClienteComponent implements OnInit {
       this.userId = +storedUserId; 
       this.loadUserData(this.userId);
     } else {
+      this.createUserDataForm();
       console.log('Nenhum userId encontrado no sessionStorage.');
     }
     // this.loadUserData(this.userId);
@@ -40,9 +41,9 @@ export class PerfilClienteComponent implements OnInit {
 
   createUserDataForm(){
     this.userDataForm = this.formBuilder.group({
-      username: [{ value: '', disabled: true }, Validators.required],
-      name: [{ value: '', disabled: true }, Validators.required],
-      documentNumber: [{ value: '', disabled: true }, Validators.required],
+      username: [{ value: this.userData.username, disabled: true }, Validators.required],
+      name: [{ value: this.userData.name, disabled: true }, Validators.required],
+      documentNumber: [{ value: this.userData.documentNumber, disabled: true }, Validators.required],
       password: [{ value: '', disabled: false }, Validators.required],
       confirmPassword: [{ value: '', disabled: false }, Validators.required]
     });
